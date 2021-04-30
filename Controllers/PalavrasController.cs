ad792ea02e3fd3d5040961b4f3e2600480718f5b
FranzFialho <franzfialho@gmail.com>
2021-04-30 01:32:21 -0300

    Links de Atualizar e Deletar

--- Controllers/PalavrasController.cs
+++ Controllers/PalavrasController.cs
@@ -25,17 +25,22 @@ namespace MimicAPI.Controllers
 
 
         [Route("")]
+
         [HttpGet]
         public ActionResult ObterTodas([FromQuery] PalavraUrlQuery query)
         {
             var item = _repository.Obterpalavras(query);
 
-            if (query.PagNumero > item.Paginacao.TotalPaginas)
+            if (item.Count == 0)
             {
                 return NotFound();
             }
 
-            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(item.Paginacao));
+            if (item.Paginacao != null)
+            {
+                Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(item.Paginacao));
+            }
+               
             return Ok(item.ToList());
         }
 
@@ -59,51 +64,51 @@ namespace MimicAPI.Controllers
             palavraDTO.Links.Add(new LinkDTO("delete", Url.Link("DeletarPalavra", new { id = palavraDTO.Id }), "DELETE"));
 
             return Ok(palavraDTO);
-    }
-
-
-    [Route("")]
-    [HttpPost]
-    public ActionResult Cadastrar([FromBody] Palavra palavra)
-    {
-        _repository.Cadastrar(palavra);
-
-        return Created($"/api/palavra/{palavra.Id}", palavra);
-    }
-
-
-
-    [HttpPut("{id:int}", Name = "AtualizarPalavra")]
-    public ActionResult Atualizar(int id, [FromBody] Palavra palavra)
-    {
-        var obj = _repository.Obter(id);
-
-        if (obj == null)
-        {
-            return NotFound();
         }
 
-        palavra.Id = id;
-        _repository.Atualizar(palavra);
 
-        return Ok();
-    }
-
-
-
-    [HttpPut("{id:int}", Name = "DeletarPalavra")]
-    public ActionResult Deletar(int id)
-    {
-        var palavra = _repository.Obter(id);
-
-        if (palavra == null)
+        [Route("")]
+        [HttpPost]
+        public ActionResult Cadastrar([FromBody] Palavra palavra)
         {
-            return NotFound();
+            _repository.Cadastrar(palavra);
+
+            return Created($"/api/palavra/{palavra.Id}", palavra);
         }
 
-        _repository.Deletar(id);
 
-        return NoContent();
+
+        [HttpPut("{id:int}", Name = "AtualizarPalavra")]
+        public ActionResult Atualizar(int id, [FromBody] Palavra palavra)
+        {
+            var obj = _repository.Obter(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            palavra.Id = id;
+            _repository.Atualizar(palavra);
+
+            return Ok();
+        }
+
+
+
+        [HttpPut("{id:int}", Name = "DeletarPalavra")]
+        public ActionResult Deletar(int id)
+        {
+            var palavra = _repository.Obter(id);
+
+            if (palavra == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Deletar(id);
+
+            return NoContent();
+        }
     }
-}
 }
